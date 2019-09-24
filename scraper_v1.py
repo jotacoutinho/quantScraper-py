4a582c79a54e520e700617eb61456eddc156579c
jpscoutinho <jotapedrocoutinho@hotmail.com>
2019-09-24 00:16:49 -0300

    Version working fine, but needs adjustments (thresholds)

--- scraper_v1.py
+++ scraper_v1.py
@@ -40,13 +40,13 @@ username = browser.find_element_by_name("email")
 password = browser.find_element_by_name("password")
 
 username.send_keys("matheusbaumbachnascimento@gmail.com")
-password.send_keys("*********")
+password.send_keys("********")
 
 keyboard.press(Key.enter)
 keyboard.release(Key.enter)
 
 # Step 3: manual filter configuration
-proxy.new_har("charts", options={'captureHeaders':True, 'captureContent':True, 'captureBinaryContent':True})
+proxy.new_har("residue", options={'captureHeaders':True, 'captureContent':True, 'captureBinaryContent':True})
 try:
     input("Press enter to continue after selecting a filter and loading the list of pairs ")
 except SyntaxError:
@@ -69,7 +69,7 @@ if browser.find_elements_by_css_selector("material-icons").count != 0:
                 numOfPairs = numOfPairs + 1
                 time.sleep(2)
                 #FIXME: remove this to get info from all pairs
-                stopGettingPairs = True
+                #stopGettingPairs = True
         index = index + 1
 
 innerHTML = browser.execute_script("return document.body.innerHTML")
@@ -91,6 +91,22 @@ currentPreferedPeriod = 0
 
 # decision variables
 maxBetaDelta = 2
+maxBetaRotVolDelta = float(25)
+maxResidueVolDelta = float(9)
+
+residueAvgTrigger = False
+firstChartRising = False
+secondChartRising = False
+chartsMatch = False
+isBetaRotationLow = False
+isResidueVolHigh = False
+alreadyGotBetaCharts = False
+
+firstChartRisingArray = []
+betaChartsArray = []
+selectedPairs = []
+currentSelectedPair = []
+
 
 with open('rawOutput.txt', 'r') as myFile:
     line = myFile.readline()
@@ -121,6 +137,7 @@ with open('rawOutput.txt', 'r') as myFile:
                 # getting green line
                 if (line.find('rgb(') != -1):
                     currentPreferedPeriod = periodCount
+                    #print(currentPreferedPeriod)
                 
                 # period
                 while (readingCount < numOfParams):
@@ -146,55 +163,195 @@ with open('rawOutput.txt', 'r') as myFile:
             currentPeriods = []
 
 # Step 5b: get chart data for further analysis
+            # beta rotation chart (same for every period)
+            time.sleep(5)
+            
+            if(alreadyGotBetaCharts == False):
+                for ent in proxy.har['log']['entries']:
+                    _url = ent['request']['url']
+                    if(_url == "https://portal.quantgo.com.br/api/operations/coefficient_graph?"):
+                        _response = ent['response']
+                        _content = _response['content']
+                        _chartInfo = _content['text']
+
+                        decodedInfo = base64.b64decode(_chartInfo)
+                        chartData = brotli.decompress(decodedInfo)
+                        chartJson = json.loads(chartData)
+                        betaChartsArray.append(chartJson)
+                
+                print(betaChartsArray)
+
+                output = open("coef.har", "w") #--> this works for getting chart data
+                output.write(str(proxy.har))
+                output.close()
+
+                for chartJson in betaChartsArray:
+                    lastIndex = len(chartJson["coefficients"]) - 1
+                    if(chartJson["coefficients"][lastIndex - 1] < chartJson["coefficients"][lastIndex]):
+                        firstChartRising = True
+                        #FIXME
+                        firstChartRisingArray.append(True)
+                    else:  
+                        firstChartRising = False
+                        #FIXME
+                        firstChartRisingArray.append(False)
+                print(firstChartRisingArray)
+                alreadyGotBetaCharts = True
+
             # Decision making: only analysing pairs with low beta variation
             sortedByBeta = sorted(pairs[pairIndex - 1].periods, key=lambda x: (x["beta"]), reverse=True)
-            if ((sortedByBeta[0].beta - sortedByBeta[periodCount - 1].beta) < maxBetaDelta):
+            currentBetaDelta = sortedByBeta[0].beta - sortedByBeta[periodCount - 1].beta
+            if (currentBetaDelta < maxBetaDelta):
 
                 try:
                     input("Press enter to extract data from a chart")
                 except SyntaxError:
                     pass
 
-                proxy.new_har("charts", options={'captureHeaders':True, 'captureContent':True, 'captureBinaryContent':True})
+                proxy.new_har("residue", options={'captureHeaders':True, 'captureContent':True, 'captureBinaryContent':True})
                 
-                #sorting perios by adf and fisher min
+                #sorting periods by adf and fisher min, letting the green period be the first one
                 rankedPeriods = sorted(pairs[pairIndex - 1].periods, key=lambda x: (x["adf"], x["fisherMin"]), reverse=True)
 
-                #click to open chart for prefered period
-                elements = 0
-                for element in browser.find_elements_by_class_name("material-icons"):
-                    if (elements == 11 + 2 * (1 + currentPreferedPeriod)):
-                        element.click()
-                    elements = elements + 1
+                #for period in rankedPeriods:
+                    #print(period.periodId)
 
-                time.sleep(5)
+                rankedPeriods.insert(0, pairs[pairIndex - 1].periods[currentPreferedPeriod])
+                rankedPeriods.reverse()
+                rankedPeriods.remove(pairs[pairIndex - 1].periods[currentPreferedPeriod])
+                rankedPeriods.reverse()
 
-                for ent in proxy.har['log']['entries']:
-                    _url = ent['request']['url']
-                    if(_url == "https://portal.quantgo.com.br/api/operations/residue_graph?"):
-                        _response = ent['response']
-                        _content = _response['content']
-                        #print(_content)
-                        _chartInfo = _content['text']
-                        #print(_chartInfo)
+                #print('now favorite first')
 
-                decodedInfo = base64.b64decode(_chartInfo)
-                chartData = brotli.decompress(decodedInfo)
-                chartJson = json.loads(chartData)
-                #print(chartJson["average"])
-                #result = json.dumps(proxy.har, ensure_ascii=False)
+                #for period in rankedPeriods:
+                    #print(period.periodId)
 
-                #time.sleep(10)
-                
-                output = open("chart.har", "w") #--> this works for getting chart data
-                output.write(str(proxy.har))
-                output.close()
-
-                server.stop() 
-
+                didSelectPeriod = False
+                chartAnalysisCount = 0
                 # if prefered fails
-                #for period in rankedPeriods.count:
+                # for period in rankedPeriods.count:
                     #extract chart data for pairs[pairIndex - 1].periods[perdiod["periodId"]]
+                while(didSelectPeriod == False and chartAnalysisCount < periodCount):
+                    #click to open chart for prefered period
+                    elements = 0
+                    for element in browser.find_elements_by_class_name("material-icons"):
+                        if (elements == 10 + pairIndex + 2 * (rankedPeriods[chartAnalysisCount].periodId + 1)):
+                            element.click()
+                            break
+                        elements = elements + 1
+
+                    time.sleep(5)
+
+                    for ent in proxy.har['log']['entries']:
+                        _url = ent['request']['url']
+                        if(_url == "https://portal.quantgo.com.br/api/operations/residue_graph?"):
+                            _response = ent['response']
+                            _content = _response['content']
+                            #print(_content)
+                            _chartInfo = _content['text']
+                            #print(_chartInfo)
+
+                    decodedInfo = base64.b64decode(_chartInfo)
+                    chartData = brotli.decompress(decodedInfo)
+                    chartJson = json.loads(chartData)
+                    #thats the main data we want from this chart
+                    
+                    lastIndex = len(chartJson["residueStandardDeviation"]) - 1
+                    if(chartJson["residueStandardDeviation"][lastIndex - 1] < chartJson["residueStandardDeviation"][lastIndex]):
+                        secondChartRising = True 
+
+                    #result = json.dumps(proxy.har, ensure_ascii=False)
+
+                    #time.sleep(10)
+                    
+                    output = open("chart.har", "w") #--> this works for getting chart data
+                    output.write(str(proxy.har))
+                    output.close()
+
+                    server.stop() 
+
+                    htmlWithChart = open("withChart.txt", "w")
+                    html = browser.execute_script("return document.body.innerHTML")
+                    htmlWithChart.write(html)
+                    htmlWithChart.close()
+
+                    betaRotVol = []
+                    residueVol = []
+
+                    htmlWithChart = open("withChart.txt", "r")
+                    chartLine = htmlWithChart.readline()
+                    while(chartLine):
+                        chartLine = htmlWithChart.readline()
+                        if(chartLine.find('<table _ngcontent-c5="" class="table table-hover">') != -1):
+                            jumpLines(htmlWithChart, 11)
+                            
+                            betaRotVol.append(float(htmlWithChart.readline().split(';')[1][2:][:-7].replace(',', '.')))
+                            betaRotVol.append(float(htmlWithChart.readline().split(';')[1][2:][:-7].replace(',', '.')))
+                            betaRotVol.append(float(htmlWithChart.readline().split(';')[1][2:][:-7].replace(',', '.')))
+                            betaRotVol.append(float(htmlWithChart.readline().split(';')[1][2:][:-7].replace(',', '.')))
+
+                            jumpLines(htmlWithChart, 4)
+
+                            residueVol.append(float(htmlWithChart.readline().split(';')[1][2:][:-7].replace(',', '.')))
+                            residueVol.append(float(htmlWithChart.readline().split(';')[1][2:][:-7].replace(',', '.')))
+                            residueVol.append(float(htmlWithChart.readline().split(';')[1][2:][:-7].replace(',', '.')))
+                            residueVol.append(float(htmlWithChart.readline().split(';')[1][2:][:-7].replace(',', '.')))
+
+                            sortedBetaRot = sorted(betaRotVol)
+                            sortedResidue = sorted(residueVol)
+
+                            print(sortedBetaRot[3] - sortedBetaRot[0])
+                            print(sortedResidue[3] - sortedResidue[0])
+
+                            if((sortedBetaRot[3] - sortedBetaRot[0]) < maxBetaRotVolDelta):
+                                isBetaRotationLow = True
+                            if((sortedResidue[3] - sortedResidue[0]) > maxResidueVolDelta):
+                                isResidueVolHigh = True
+                            
+
+                            print("Decision outputs: beta rotation, residue and matching charts.")
+                            print(sortedBetaRot)
+                            print(isBetaRotationLow)
+                            print(sortedResidue)
+                            print(isResidueVolHigh)
+                            
+                    if(firstChartRising != secondChartRising):
+                        chartsMatch = True
+                    
+                    betaRotVol = []
+                    residueVol = []
+                    print(chartsMatch)
+
+                    if(chartsMatch and isResidueVolHigh and isBetaRotationLow):
+                        didSelectPeriod = True
+                        currentSelectedPair = [pairIndex - 1, rankedPeriods[chartAnalysisCount], currentBetaDelta]
+                    else:
+                        print("Go to next period...")
+                        #hide periods
+                        #elementIndex = 10 + pairIndex
+                        #print(elementIndex)
+                        elementIndex = 0
+                        for element in browser.find_elements_by_class_name("material-icons"):
+                            if (elementIndex == elements):
+                                element.click()
+                                break
+                            elementIndex = elementIndex + 1  
+
+                    chartAnalysisCount = chartAnalysisCount + 1
+                    
+                if(currentSelectedPair != []):
+                    selectedPairs.append(currentSelectedPair)
+                    print(selectedPairs)
+            
+            #hide periods
+            elementIndex = 10 + pairIndex
+            #print(elementIndex)
+            elements = 0
+            for element in browser.find_elements_by_class_name("material-icons"):
+                if (elements == elementIndex):
+                    element.click()
+                    break
+                elements = elements + 1
 
 output = open("output.txt", "w")
 for pair in pairs:
